9fa3cb4f6c4070687e3826de5ac3131d6535c531
Emily Jimenez <erjdhs@outlook.com>
2025-06-20 13:00:31 -0400

    push test

--- example_emily.R
+++ example_emily.R
@@ -2,12 +2,17 @@
 # Data we need: 
 # Population totals (by county preferred)
 
-# Gathering population data fro each county over time 
+# Gathering population data from each county over time 
+
+
+# API key
+census_api_key("afc5cb6a204acc0021390161be24a3b933a2c4ea", install = TRUE)
+
 
 # Global Factors
 key <- NULL # hidden but shows API key 
 # for security, this key should be loaded in each session
-year <- 2022 # input year of data 
+# year <- 2022 # input year of data 
 acsx <- "acs5" # Select version and year of acs data
 output <- "tidy" # format of data output
 state <- 55 # FIPS code location of interest
@@ -17,63 +22,6 @@ geography <- "county" # location specific data
 show_call <- FALSE # if TRUE, display call made to Census API
 cache_table <- TRUE # whether or not to cache table names for faster future access. Defaults to FALSE
 
-# Table B01001H001 Estimate!!Total: Sex by Age White Alone (non-Hispanic)
-white_alone_pop_county <- get_acs(
-  geography = geography, 
-  variables = "B01001H_001", 
-  year = year, 
-  state = state,
-  cache_table = cache_table
-  ) 
-
-# Table B01001B_001 Estimate!!Total: Sex by Age Black African American Alone
-black_alone_pop_county <- get_acs(
-  geography = geography, 
-  variables = "B01001B_001", 
-  year = year, 
-  state = state,
-  cache_table = cache_table
-)
-
-# Table B01001C_001  Estimate!!Total: Sex by Age American Indian and Alaskan Native Alone
-american_indian_alaskan_native_alone_pop_county <- get_acs(
-  geography = geography, 
-  variables = "B01001C_001", 
-  year = year, 
-  state = state,
-  cache_table = cache_table
-)
-
-
-get_acs(
-  geography = geography, 
-  variables = c("B01001A_001", "B01001B_001", "B01001C_001"),
-  year = year, 
-  state = state,
-  cache_table = cache_table
-) %>% 
-  mutate(
-    race = case_when(
-      variable = "B01001A_001" ~ "white", 
-      variable = "B01001B_001" ~ "black or african american",
-      variable = "B01001C_001" ~ "american indian and alaskan native")
-    )
-
-library(forcats)
-?forcats::fct_recode
-
-get_acs(
-  geography = geography, 
-  variables = c("B01001A_001", "B01001B_001", "B01001C_001"),
-  year = year, 
-  state = state,
-  cache_table = cache_table
-) %>% 
-  mutate(race = forcats::fct_recode(variable,
-    white = "B01001A_001", 
-    black = "B01001B_001", 
-    aian = "B01001C_001"
-  ))
 
 
 get_acs(
